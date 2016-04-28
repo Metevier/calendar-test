99e8177c17d616b1f60100c094f0b3656e019797
Tyler Metevier <tyler@collabramusic.com>
2016-04-28 13:05:34 -0400

    Add logic to generate time ranges

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var App = React.createClass({
 
-  today: moment().format('MM/DD/YYYY'),
+  today: moment().startOf('day').format('MM/DD/YYYY').toString(),
 
   getInitialState: function () {
     return {
@@ -21,6 +21,9 @@ var App = React.createClass({
                    numDays={numDays}
                    countryCode={countryCode}
                    updateField={this.updateField}/>
+        <Calendar startDate={startDate}
+                  numDays={numDays}
+                  countryCode={countryCode}/>
       </div>
     );
   },
@@ -42,7 +45,7 @@ var UserInput = React.createClass({
           <input id="startDate"
                  className="form-control"
                  onChange={this.props.updateField.bind(null, 'startDate')}
-                 value={this.props.startDate.toString()}/>
+                 value={this.props.startDate}/>
         </div>
 
         <div className="num-days">
@@ -68,10 +71,74 @@ var UserInput = React.createClass({
 var Calendar = React.createClass({
 
   render: function() {
+    var months = this.seedCalendar(this.props.startDate, this.props.numDays, this.props.countryCode);
+    console.log(months);
+    return (
+      <div>
+
+      </div>
+    );
+  },
+
+  //Here we will do most of our logic
+  seedCalendar: function (startDate, numDays, countryCode) {
+    var startMoment = moment(new Date(startDate)); //Transform string into moment object
+    var endMoment = moment(startMoment).add(numDays - 1, 'days'); //Subtract 1 because we need a number not a range.
+    var numMonths = Math.ceil(endMoment.diff(startMoment, 'months', true)) + 1; //Add 1 because we need a range
+    
+    //Get start and end for each month in between the start and end period
+    var months = [];
+    for (var i = 0; i < numMonths; i++) {
+      var month = {};
+      var monthMoment = moment(startMoment).startOf('month').add(i, 'months'); //Get the next month
+
+      //We check if the month and year are the same as the start or end date,
+      //if so we set that month to have the same start or end date, respectively
+      if (startMoment.isSame(monthMoment, 'year') && startMoment.isSame(monthMoment, 'month')) {
+        month.startMoment = startMoment;
+      } else {
+        month.startMoment = moment(monthMoment).startOf('month');
+      }
+
+      if (endMoment.isSame(monthMoment, 'year') && endMoment.isSame(monthMoment, 'month')) {
+        month.endMoment = endMoment;
+      } else {
+        month.endMoment = moment(monthMoment).endOf('month');
+      }
+
+      months.push(month);
+    }
+    return months;
+  }
+});
+
+var CalendarMonth = React.createClass({
+
+  render: function() {
     return (
       <div></div>
     );
   }
 });
+
+var CalendarRow = React.createClass({
+
+  render: function() {
+    return (
+      <div></div>
+    );
+  }
+});
+
+var CalendarCell = React.createClass({
+
+  render: function() {
+    return (
+      <div></div>
+    );
+  }
+});
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
